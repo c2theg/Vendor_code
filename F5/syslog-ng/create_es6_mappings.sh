f2999cb4b00d0b547ca57fb9c0279dcaa2bf2aeb
Chris Gray <christophermjgray@gmail.com>
2018-03-09 10:23:47 -0500

    Update create_es6_mappings.sh

--- F5/syslog-ng/create_es6_mappings.sh
+++ F5/syslog-ng/create_es6_mappings.sh
@@ -103,6 +103,116 @@ curl -H 'Content-Type: application/json' -X PUT localhost:9200/_template/bigip.l
 }'
 
 
+#
+#
+#
+# ---  Create the template for indexing the HTTP logs
+#
+#
+#
+echo "\r\n \r\n Creating HTTP Logs ES Mapping....  \r\n \r\n "
 
+curl -H 'Content-Type: application/json' -X PUT localhost:9200/_template/http.logs -d '
+{  
+   "template":"http.logs*",
+   "settings":{  
+      "number_of_shards":4
+   },
+   "mappings":{  
+      "logs":{  
+         "properties":{  
+            "@timestamp":{  
+               "type":"date",
+               "format":"strict_date_optional_time||epoch_millis"
+            },
+            "@version":{  
+               "type":"text"
+            },
+            "agent":{  
+               "type":"text",
+               "analyzer":"english",
+               "fields":{  
+                  "raw":{  
+                     "type":"text",
+                     "index":"false"
+                  }
+               }
+            },
+            "bytes_received":{  
+               "type":"integer"
+            },
+            "client_ip":{  
+               "type":"ip"
+            },
+            "client_port":{  
+               "type":"text"
+            },
+            "host":{  
+               "type":"text"
+            },
+            "http_status":{  
+               "type":"text"
+            },
+            "http_version":{  
+               "type":"text"
+            },
+            "message":{  
+               "type":"text"
+            },
+            "path":{  
+               "type":"text"
+            },
+            "referrer":{  
+               "type":"text",
+               "analyzer":"english",
+               "fields":{  
+                  "raw":{  
+                     "type":"text",
+                     "index":"false"
+                  }
+               }
+            },
+            "response_time":{  
+               "type":"integer"
+            },
+            "server_ip":{  
+               "type":"ip"
+            },
+            "server_port":{  
+               "type":"text"
+            },
+            "syslog_hostname":{  
+               "type":"text",
+               "analyzer":"english",
+               "fields":{  
+                  "raw":{  
+                     "type":"text",
+                     "index":"false"
+                  }
+               }
+            },
+            "syslog_timestamp":{  
+               "type":"text"
+            },
+            "uri_path":{  
+               "type":"text",
+               "analyzer":"english",
+               "fields":{  
+                  "raw":{  
+                     "type":"text",
+                     "index":"false"
+                  }
+               }
+            },
+            "virtual_port":{  
+               "type":"text"
+            },
+            "vitual_ip":{  
+               "type":"ip"
+            }
+         }
+      }
+   }
+}'
 
 
