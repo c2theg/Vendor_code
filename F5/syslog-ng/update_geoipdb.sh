edb0f22ff299f60d04123e72b47064deb6314f5d
Chris Gray <christophermjgray@gmail.com>
2018-03-21 16:14:26 -0400

    Create update_geoipdb.sh

---
+++ F5/syslog-ng/update_geoipdb.sh
@@ -0,0 +1,8 @@
+# example file: /etc/elasticsearch/ingest-geoip/GeoLite2-Country.mmdb.gz
+
+#download files from: https://dev.maxmind.com/geoip/geoip2/geolite2/
+#untar files 
+# move files to: /etc/elasticsearch/ingest-geoip/
+
+
+wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
