f5db1fcb2d983f4c2f7cf6413346d9489c0edf75
Chris Gray <christophermjgray@gmail.com>
2018-03-21 16:15:22 -0400

    Update update_geoipdb.sh

--- F5/syslog-ng/update_geoipdb.sh
+++ F5/syslog-ng/update_geoipdb.sh
@@ -1,3 +1,29 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  3/21/2018
+\r\n \r\n
+This is meant for Ubuntu 16.04+  \r\n \r\n"
+#--------------------------------------------------------------------------------------
+
 # example file: /etc/elasticsearch/ingest-geoip/GeoLite2-Country.mmdb.gz
 
 #download files from: https://dev.maxmind.com/geoip/geoip2/geolite2/
