536c584f98cd7fff572e9dcf5598167c15d0879d
Chris Gray <christophermjgray@gmail.com>
2018-03-21 16:46:57 -0400

    Update update_geoipdb.sh

--- F5/syslog-ng/update_geoipdb.sh
+++ F5/syslog-ng/update_geoipdb.sh
@@ -18,33 +18,56 @@ echo "
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
 \r\n \r\n
-Version:  0.0.5                             \r\n
+Version:  0.1.0                             \r\n
 Last Updated:  3/21/2018
 \r\n \r\n
 This is meant for Ubuntu 16.04+  \r\n \r\n"
 #--------------------------------------------------------------------------------------
 
-# example file: /etc/elasticsearch/ingest-geoip/GeoLite2-Country.mmdb.gz
-
-#download files from: https://dev.maxmind.com/geoip/geoip2/geolite2/
-#untar files 
-# move files to: /etc/elasticsearch/ingest-geoip/
-
-echo "Downloading latest file from Maxmind.com....  \r\n \r\n"
+#--------------------- City ---------------------
+echo "Downloading latest City database file from Maxmind.com....  \r\n \r\n"
 wget -O "GeoLite2-City.tar.gz" "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz"
 wait
 
-echo "\r\n Removing files \r\n "
+echo "\r\n Removing files.. "
 rm /etc/elasticsearch/ingest-geoip/README.txt
 rm /etc/elasticsearch/ingest-geoip/LICENSE.txt
 rm /etc/elasticsearch/ingest-geoip/COPYRIGHT.txt
 
-echo "DONE! Uncompressing.. \r\n "
+echo "DONE! \r\n \r\n Uncompressing.. \r\n "
 tar xvzf GeoLite2-City.tar.gz --strip-components=1 -C /etc/elasticsearch/ingest-geoip/
+echo "Done! \r\n \r\n"
+wait
+
+#--------------------- Country ---------------------
+echo "Downloading latest Country database file from Maxmind.com....  \r\n \r\n"
+wget -O "GeoLite2-Country.tar.gz" "http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz"
+wait
+
+echo "\r\n Removing files.. "
+rm /etc/elasticsearch/ingest-geoip/README.txt
+rm /etc/elasticsearch/ingest-geoip/LICENSE.txt
+rm /etc/elasticsearch/ingest-geoip/COPYRIGHT.txt
+
+echo "DONE! \r\n \r\n Uncompressing.. \r\n "
+tar xvzf GeoLite2-Country.tar.gz --strip-components=1 -C /etc/elasticsearch/ingest-geoip/
+echo "Done! \r\n \r\n"
+wait
+
+#--------------------- ASN ---------------------
+echo "Downloading latest ASN database file from Maxmind.com....  \r\n \r\n"
+wget -O "GeoLite2-ASN.tar.gz" "http://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz"
+wait
+
+echo "\r\n Removing files.. "
+rm /etc/elasticsearch/ingest-geoip/README.txt
+rm /etc/elasticsearch/ingest-geoip/LICENSE.txt
+rm /etc/elasticsearch/ingest-geoip/COPYRIGHT.txt
+
+echo "DONE! \r\n \r\n Uncompressing.. \r\n "
+tar xvzf GeoLite2-ASN.tar.gz --strip-components=1 -C /etc/elasticsearch/ingest-geoip/
+echo "Done! \r\n \r\n"
 wait
 
 
-
-
-   
 echo "Done! \r\n \r\n"
