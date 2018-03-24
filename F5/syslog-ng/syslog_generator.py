e4cc0720b20edb5739728048872cb4f1fca2f116
Chris Gray <christophermjgray@gmail.com>
2018-03-24 12:44:29 -0400

    Update syslog_generator.py

--- F5/syslog-ng/syslog_generator.py
+++ F5/syslog-ng/syslog_generator.py
@@ -4,13 +4,18 @@ my_logger = logging.getLogger('MyLogger')
 my_logger.setLevel(logging.INFO)
 
 #Define SyslogHandler
-syslog_server = '192.168.1.2'
+syslog_server = '192.168.1.210'
 syslog_port = 1514
 
 handler = logging.handlers.SysLogHandler(address = (syslog_server,syslog_port))
 my_logger.addHandler(handler)
 
-#Example: We will pass values from a List
-List1 = ['test1','test2','test3']
+#A list of messages to send
+List1 = [
+  'test1',
+  'test2',
+  'test3'
+]
+
 for row in List1:
-  my_logger.info("I was in " +List1[0])
+  my_logger.info(List1[0])
