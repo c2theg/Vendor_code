060a7c35b2a60ab4b86317993731e396e6aab0d5
Chris Gray <christophermjgray@gmail.com>
2018-03-24 21:35:17 -0400

    Update syslog_generator.py

--- F5/syslog-ng/syslog_generator.py
+++ F5/syslog-ng/syslog_generator.py
@@ -1,4 +1,4 @@
-''' - Version 0.0.5  - 3/24/18 '''
+# --- Version 0.0.6 - 3/24/18
 import logging
 import logging.handlers
 my_logger = logging.getLogger('MyLogger')
@@ -7,12 +7,14 @@ my_logger.setLevel(logging.INFO)
 #Define SyslogHandler
 syslog_server = '192.168.1.210'
 syslog_port = 1514
+fs_syslog_lines_to_send = 'syslog_lines.txt'
 
 print "\r\n \r\n About to send Syslog message to: ", syslog_server, ":", syslog_port, "... \r\n \r\n"
 handler = logging.handlers.SysLogHandler(address = (syslog_server,syslog_port))
 my_logger.addHandler(handler)
 
 #A list of messages to send
+'''
 List1 = [
   "<134>1 2018-03-22T08:01:19.969759-07:00 bigip1.dnstest.lab tmm 16694 23003139 [F5@12276 action=\"Drop\" attack_type=\"MX\" hostname=\"bigip1.dnstest.lab\" bigip_mgmt_ip=\"192.168.1.100\" context_name=\"/Common/dns_udp_listener\" date_time=\"Mar 22 2018 08:01:19\" dest_ip=\"162.159.25.175\" dest_port=\"53\" device_product=\"Advanced Firewall Module\" device_vendor=\"F5\" device_version=\"13.1.0.1.0.0.8\" dns_query_name=\"mountaineerpublishing.com\" dns_query_type=\"MX\" errdefs_msgno=\"23003139\" errdefs_msg_name=\"DNS Event\" flow_id=\"0005565750f68689\" severity=\"5\" partition_name=\"Common\" route_domain=\"0\" source_ip=\"10.10.0.50\" source_port=\"48560\" vlan=\"/Common/inside\"] \"Mar 22 2018 08:01:19\",\"192.168.1.100\",\"bigip1.dnstest.lab\",\"/Common/dns_udp_listener\",\"/Common/inside\",\"MX\",\"mountaineerpublishing.com\",\"MX\",\"Drop\",\"10.10.0.50\",\"162.159.25.175\",\"48560\",\"53\",\"0\",\"0005565750f68689\"",
   'test2',
@@ -24,6 +26,12 @@ while i < len(List1):
   print "Sending [", List1[i], "]... \r\n"
   my_logger.info(List1[i])
   i += 1
-  
-  
+'''
+
+with open(fs_syslog_lines_to_send) as f:
+    fs_lines = f.readlines()
+    print "Sending: [", fs_lines, "]... \r\n"
+    my_logger.info(fs_lines)
+
+
 print "\r\n Done! \r\n "
