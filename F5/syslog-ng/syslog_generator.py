27386cfa7323376ece06382b91273b430018dcfe
Chris Gray <christophermjgray@gmail.com>
2018-03-24 20:52:14 -0400

    Update syslog_generator.py

--- F5/syslog-ng/syslog_generator.py
+++ F5/syslog-ng/syslog_generator.py
@@ -7,6 +7,7 @@ my_logger.setLevel(logging.INFO)
 syslog_server = '192.168.1.210'
 syslog_port = 1514
 
+print "About to send Syslog message to: "+syslog_server+":"+syslog_port+"... \r\n \r\n"
 handler = logging.handlers.SysLogHandler(address = (syslog_server,syslog_port))
 my_logger.addHandler(handler)
 
@@ -18,4 +19,7 @@ List1 = [
 ]
 
 for row in List1:
+  print "Sending ["+ List1[0] + "]... \r\n"
   my_logger.info(List1[0])
+
+print "Done! \r\n "
