985c9e250f22e1e5e15fe7f64b28cfa58cda7f1a
Chris Gray <christophermjgray@gmail.com>
2018-03-27 10:36:30 -0700

    Update create_es6_mappings_template.sh

--- F5/syslog-ng/create_es6_mappings_template.sh
+++ F5/syslog-ng/create_es6_mappings_template.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 # Christopher Gray
-# Version 2.2.1
+# Version 2.2.2
 #  3-27-18
-#
+
+if [ "$#" -eq  "0" ]
+   then
+     server_ip=127.0.0.1
+else
+     echo "Server is $1"
+fi
 
 if [ -s "es_mapping_bigip.json" ]
 then
@@ -24,52 +30,52 @@ wait
 echo "\r\n \r\n "
 echo "Deleting existing ES indexs if present....  \r\n \r\n "
 echo "\r\n BigIP ... \r\n "
-curl -XDELETE 'localhost:9200/bigip*?pretty'
+curl -XDELETE $1:9200/bigip*?pretty
 echo "\r\n HTTP ... \r\n "
-curl -XDELETE 'localhost:9200/http*?pretty'
+curl -XDELETE $1:9200/http*?pretty
 echo "\r\n DDoS ... \r\n "
-curl -XDELETE 'localhost:9200/ddos*?pretty'
+curl -XDELETE $1:9200/ddos*?pretty
 echo "\r\n DNS ... \r\n "
-curl -XDELETE 'localhost:9200/dns*?pretty'
+curl -XDELETE $1:9200/dns*?pretty
 wait
 
 echo "\r\n TEMPLATES ... \r\n "
 
 echo "\r\n BigIP ... \r\n "
-curl -XDELETE 'localhost:9200/_template/bigip*?pretty'
+curl -XDELETE $1:9200/_template/bigip*?pretty
 echo "\r\n HTTP ... \r\n "
 wait
-curl -XDELETE 'localhost:9200/_template/http*?pretty'
+curl -XDELETE $1:9200/_template/http*?pretty
 echo "\r\n DDoS ... \r\n "
 wait
-curl -XDELETE 'localhost:9200/_template/ddos*?pretty'
+curl -XDELETE $1:9200/_template/ddos*?pretty
 wait
 echo "\r\n DNS ... \r\n "
-curl -XDELETE 'localhost:9200/_template/dns*?pretty'
+curl -XDELETE $1:9200/_template/dns*?pretty
 wait
 
 echo "\r\n \r\n "
 #curl -H 'Content-Type: application/json' -X PUT localhost:9200/_template/bigip.logs -d ''
 echo "Creating BigIP Logs ES 6 Mapping....  \r\n \r\n "
-curl -s -XPOST localhost:9200/_bulk --data-binary @es_mapping_bigip.json
+curl -s -XPOST $1:9200/_bulk --data-binary @es_mapping_bigip.json
 
 echo "\r\n \r\n Creating HTTP Logs ES 6 Mapping....  \r\n \r\n "
-curl -s -XPOST localhost:9200/_bulk --data-binary @es_mapping_http.json
+curl -s -XPOST $1:9200/_bulk --data-binary @es_mapping_http.json
 
 echo "\r\n \r\n Creating DDoS Logs ES 6 Mapping....  \r\n \r\n "
-curl -s -XPOST localhost:9200/_bulk --data-binary @es_mapping_ddos.json
+curl -s -XPOST $1:9200/_bulk --data-binary @es_mapping_ddos.json
 
 echo "\r\n \r\n Creating DDoS Logs ES 6 Mapping....  \r\n \r\n "
-curl -s -XPOST localhost:9200/_bulk --data-binary @es_mapping_dns.json
+curl -s -XPOST $1:9200/_bulk --data-binary @es_mapping_dns.json
 echo "DONE! \r\n \r\n"
 
 
 echo "\r\n \r\n Get ES Stats:  \r\n \r\n "
-curl -XGET 'localhost:9200/_cat/indices?v&pretty'
-curl -XGET 'localhost:9200/_cat/health?v&pretty'
-curl -XGET 'localhost:9200/_cat/nodes?v&pretty'
+curl -XGET $1:9200/_cat/indices?v&pretty
+curl -XGET $1:9200/_cat/health?v&pretty
+curl -XGET $1:9200/_cat/nodes?v&pretty
 
 echo "\r\n \r\n  Cluster Templates' \r\n \r\n"
-curl -XGET 'localhost:9200/_template/*?pretty'
+curl -XGET $1:9200/_template/*?pretty
 
 echo "\r\n \r\n \r\n"
