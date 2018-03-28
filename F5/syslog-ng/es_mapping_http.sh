9a2ae21c7d79634843b414ab08cd3997170a00d5
Chris Gray <christophermjgray@gmail.com>
2018-03-28 16:09:14 -0700

    Update es_mapping_http.sh

--- F5/syslog-ng/es_mapping_http.sh
+++ F5/syslog-ng/es_mapping_http.sh
@@ -15,7 +15,16 @@ else
       echo "ES Server is set to $server_ip \r\n"
 fi
 
-curl -H 'Content-Type: application/json' -X PUT $server_ip:9200/_template/http.logs -d '
+if [ -z "$2" ]
+   then
+      echo "No port specified. Defaulting to 9200 \r\n"
+      server_port=9200
+   else
+      server_port=$2
+      #echo "port = $server_port \r\n"
+fi
+
+curl -H 'Content-Type: application/json' -X PUT $server_ip:$server_port/_template/http.logs -d '
 {
       "index_patterns":"http.logs*",
       "settings":{  
