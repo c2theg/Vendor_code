2a53fef71e9d71fc12e1a2d7678fdbc3863a18fd
Chris Gray <christophermjgray@gmail.com>
2018-03-28 13:33:29 -0700

    Update es_mapping_dns.sh

--- F5/syslog-ng/es_mapping_dns.sh
+++ F5/syslog-ng/es_mapping_dns.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Christopher Gray
-# Version 0.1.3
+# Version 0.1.4
 #  3-28-18
 
 #ElasticSearch Mapping for: F5 DNS
@@ -17,86 +17,95 @@ fi
 
 curl -H 'Content-Type: application/json' -X PUT $server_ip:9200/_template/dns.logs -d '
 {
-   "index_patterns" : "dns.logs*",
+    "index_patterns" : "dns.logs*",
     "settings" : {
-      "number_of_shards": 3,
-      "number_of_replicas" : 0,
-      "refresh_interval": "1s",
-      "index.routing.allocation.include.size": "small",
-      "index.routing.allocation.include.rack": "r1"
+        "number_of_shards": 3,
+        "number_of_replicas" : 0,
+        "refresh_interval": "1s",
+        "index.routing.allocation.include.size": "small",
+        "index.routing.allocation.include.rack": "r1"
     },
-   "mappings": {
-      "properties": {
-         "@timestamp": {
-            "type": "date",
-            "format": "strict_date_optional_time||epoch_millis"
-         },
-         "@version": {
-            "type": "string"
-         },
-         "host": {
-            "type": "string"
-         },
-         "message": {
-            "type": "string"
-         },
-         "path": {
-            "type": "string"
-         },
-         "syslog_hostname": {
-            "type": "string",
-              "analyzer": "english",
-              "fields": {
-                  "raw": {
-                      "type":  "string",
-                      "index": "not_analyzed"
-                  }
-              }
-         },
-         "Subscriber": {
-            "type": "ip"
-         },
-        "port": {
-            "type": "integer"
-         },
-         "Query": {
-            "type": "string",
-              "analyzer": "english",
-              "fields": {
-                  "raw": {
-                      "type":  "string",
-                      "index": "not_analyzed"
-                  }
-              }
-         },
-        "QueryType": {
-            "type": "string",
-              "analyzer": "english",
-              "fields": {
-                  "raw": {
-                      "type":  "string",
-                      "index": "not_analyzed"
-                  }
-              }
-         },
-         "DNS_response": {
-            "type": "string",
-              "analyzer": "english",
-              "fields": {
-                  "raw": {
-                      "type":  "string",
-                      "index": "not_analyzed"
-                  }
-              }
-         },
-         "timestamp": {
-            "type": "string"
-         }
-      }
+    "mappings": {
+        "logs": {
+            "_source": {
+                "enabled": false
+            },
+            "_meta": {
+                "version": "1.0"
+            },
+            "dynamic": false,
+            "properties":{
+                "@timestamp": {
+                    "type": "date",
+                    "format": "strict_date_optional_time||epoch_millis"
+                },
+                "@version": {
+                    "type": "string"
+                },
+                "host": {
+                    "type": "string"
+                },
+                "message": {
+                    "type": "string"
+                },
+                "path": {
+                    "type": "string"
+                },
+                "syslog_hostname": {
+                    "type": "string",
+                    "analyzer": "english",
+                    "fields": {
+                        "raw": {
+                            "type":  "string",
+                            "index": "not_analyzed"
+                        }
+                    }
+                },
+                "Subscriber": {
+                    "type": "ip"
+                },
+                "port": {
+                    "type": "integer"
+                },
+                "Query": {
+                    "type": "string",
+                    "analyzer": "english",
+                    "fields": {
+                        "raw": {
+                            "type":  "string",
+                            "index": "not_analyzed"
+                        }
+                    }
+                },
+                "QueryType": {
+                    "type": "string",
+                    "analyzer": "english",
+                    "fields": {
+                        "raw": {
+                            "type":  "string",
+                            "index": "not_analyzed"
+                        }
+                    }
+                },
+                "DNS_response": {
+                    "type": "string",
+                    "analyzer": "english",
+                    "fields": {
+                        "raw": {
+                            "type":  "string",
+                            "index": "not_analyzed"
+                        }
+                    }
+                },
+                "timestamp": {
+                    "type": "string"
+                }
+            }
+        
+        
+        
 
-
-
-
-
-   }
+        
+        }
+    }
 }'
