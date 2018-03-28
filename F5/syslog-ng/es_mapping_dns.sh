96c3ec37622ad4338b8b32d9eaaf1180324b8813
Chris Gray <christophermjgray@gmail.com>
2018-03-28 10:13:14 -0700

    Update and rename es_mapping_dns.json to es_mapping_dns.sh

--- F5/syslog-ng/es_mapping_dns.sh
+++ F5/syslog-ng/es_mapping_dns.sh
@@ -1,7 +1,23 @@
+#!/bin/sh
+# Christopher Gray
+# Version 0.1.3
+#  3-28-18
+
+#ElasticSearch Mapping for: F5 DNS
+#https://support.f5.com/kb/en-us/products/big-ip_ltm/manuals/product/bigip-external-monitoring-implementations-13-0-0/15.html#guid-87e43db0-6700-48d1-8e1b-d52e8bb6c899
+
+if [ "$#" -eq  "0" ]
+   then
+      echo "No server ip specified. Defaulting to localhost \r\n"
+      server_ip=127.0.0.1
+else
+      server_ip=$1
+      echo "ES Server is set to $server_ip \r\n"
+fi
+
+curl -H 'Content-Type: application/json' -X PUT $server_ip:9200/_template/dns.logs -d '
 {
-   "_comment": "ElasticSearch Mapping for: F5 DNS - version 0.1.2 - 3/28/18",
-   "_comment": "https://support.f5.com/kb/en-us/products/big-ip_ltm/manuals/product/bigip-external-monitoring-implementations-13-0-0/15.html#guid-87e43db0-6700-48d1-8e1b-d52e8bb6c899",
-    "index_patterns" : "dns.logs*",
+   "index_patterns" : "dns.logs*",
     "settings" : {
       "number_of_shards": 3,
       "number_of_replicas" : 0,
@@ -83,4 +99,4 @@
 
 
    }
-}
+}'
