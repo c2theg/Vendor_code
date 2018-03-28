c7a6fbcc1adfe12c95b86848679594408d9ff46c
Chris Gray <christophermjgray@gmail.com>
2018-03-28 15:51:53 -0700

    Update es_mapping_http.sh

--- F5/syslog-ng/es_mapping_http.sh
+++ F5/syslog-ng/es_mapping_http.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Christopher Gray
-# Version 0.1.3
+# Version 0.1.4
 #  3-28-18
 
 #ElasticSearch Mapping for: F5 HTTP / ASM
@@ -17,106 +17,115 @@ fi
 
 curl -H 'Content-Type: application/json' -X PUT $server_ip:9200/_template/http.logs -d '
 {
-   "index_patterns":"http.logs*",
-   "settings":{  
-      "number_of_shards": 3,
-      "number_of_replicas" : 0,
-      "refresh_interval": "1s",
-      "index.routing.allocation.include.size": "small",
-      "index.routing.allocation.include.rack": "r1"
-   },
-   "mappings":{
-         "properties":{  
-            "@timestamp":{
-               "type":"date",
-               "format":"strict_date_optional_time||epoch_millis"
-            },
-            "@version":{  
-               "type":"text"
-            },
-            "agent":{  
-               "type":"text",
-               "analyzer":"english",
-               "fields":{  
-                  "raw":{  
-                     "type":"text",
-                     "index":"false"
+      "index_patterns":"http.logs*",
+      "settings":{  
+            "number_of_shards": 3,
+            "number_of_replicas" : 0,
+            "refresh_interval": "1s",
+            "index.routing.allocation.include.size": "small",
+            "index.routing.allocation.include.rack": "r1"
+      },
+      "mappings":{
+            "logs": {
+                  "_source": {
+                        "enabled": false
+                  },
+                  "_meta": {
+                        "version": "1.0"
+                  },
+                  "dynamic": false,
+                  "properties":{
+                        "@timestamp":{
+                              "type":"date",
+                              "format":"strict_date_optional_time||epoch_millis"
+                        },
+                        "@version":{  
+                              "type":"text"
+                        },
+                        "agent":{  
+                              "type":"text",
+                              "analyzer":"english",
+                              "fields":{  
+                                    "raw":{  
+                                    "type":"text",
+                                    "index":"false"
+                                    }
+                              }
+                        },
+                        "bytes_received":{  
+                              "type":"integer"
+                        },
+                        "client_ip":{  
+                              "type":"ip"
+                        },
+                        "client_port":{  
+                              "type":"text"
+                        },
+                        "host":{  
+                              "type":"text"
+                        },
+                        "http_status":{  
+                              "type":"text"
+                        },
+                        "http_version":{  
+                              "type":"text"
+                        },
+                        "message":{  
+                              "type":"text"
+                        },
+                        "path":{  
+                              "type":"text"
+                        },
+                        "referrer":{  
+                              "type":"text",
+                              "analyzer":"english",
+                              "fields":{  
+                                    "raw":{  
+                                    "type":"text",
+                                    "index":"false"
+                                    }
+                              }
+                        },
+                        "response_time":{  
+                              "type":"integer"
+                        },
+                        "server_ip":{  
+                              "type":"ip"
+                        },
+                        "server_port":{  
+                              "type":"text"
+                        },
+                        "syslog_hostname":{  
+                              "type":"text",
+                              "analyzer":"english",
+                              "fields":{  
+                                    "raw":{  
+                                    "type":"text",
+                                    "index":"false"
+                                    }
+                              }
+                        },
+                        "syslog_timestamp":{  
+                              "type":"text"
+                        },
+                        "uri_path":{  
+                              "type":"text",
+                              "analyzer":"english",
+                              "fields":{  
+                                    "raw":{  
+                                    "type":"text",
+                                    "index":"false"
+                                    }
+                              }
+                        },
+                        "virtual_port":{  
+                              "type":"text"
+                        },
+                        "vitual_ip":{  
+                              "type":"ip"
+                        }
                   }
-               }
-            },
-            "bytes_received":{  
-               "type":"integer"
-            },
-            "client_ip":{  
-               "type":"ip"
-            },
-            "client_port":{  
-               "type":"text"
-            },
-            "host":{  
-               "type":"text"
-            },
-            "http_status":{  
-               "type":"text"
-            },
-            "http_version":{  
-               "type":"text"
-            },
-            "message":{  
-               "type":"text"
-            },
-            "path":{  
-               "type":"text"
-            },
-            "referrer":{  
-               "type":"text",
-               "analyzer":"english",
-               "fields":{  
-                  "raw":{  
-                     "type":"text",
-                     "index":"false"
-                  }
-               }
-            },
-            "response_time":{  
-               "type":"integer"
-            },
-            "server_ip":{  
-               "type":"ip"
-            },
-            "server_port":{  
-               "type":"text"
-            },
-            "syslog_hostname":{  
-               "type":"text",
-               "analyzer":"english",
-               "fields":{  
-                  "raw":{  
-                     "type":"text",
-                     "index":"false"
-                  }
-               }
-            },
-            "syslog_timestamp":{  
-               "type":"text"
-            },
-            "uri_path":{  
-               "type":"text",
-               "analyzer":"english",
-               "fields":{  
-                  "raw":{  
-                     "type":"text",
-                     "index":"false"
-                  }
-               }
-            },
-            "virtual_port":{  
-               "type":"text"
-            },
-            "vitual_ip":{  
-               "type":"ip"
             }
-         }
 
 
 
