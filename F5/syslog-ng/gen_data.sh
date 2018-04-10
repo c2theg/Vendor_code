37f538530515eaf1a75d09676f1005c51ef3bd28
Chris Gray <christophermjgray@gmail.com>
2018-04-09 22:32:45 -0400

    Update gen_data.sh

--- F5/syslog-ng/gen_data.sh
+++ F5/syslog-ng/gen_data.sh
@@ -1,1 +1,9 @@
-dnsperf -s 10.10.0.50 -d queryfile-example-current -c 20 -T 20 -l 600 -q 10000 -Q 1
+#Clean traffic: 
+dnsperf -s 10.10.0.50 -d ~/query-file-example-current -c 200 -T 10 -l 300 -q 10000 -Q 25
+
+wait
+wait
+#Flood: 
+dnsperf -s 10.10.0.50 -d ~/query-file-example-current -c 200 -T 10 -l 300 -q 10000 -Q 1000
+
+
