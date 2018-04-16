b69e108ca906e5e1c04e2324631fe8ff48c6544e
Chris Gray <christophermjgray@gmail.com>
2018-04-16 15:34:28 -0700

    Update gen_data.sh

--- F5/syslog-ng/gen_data.sh
+++ F5/syslog-ng/gen_data.sh
@@ -7,3 +7,7 @@ wait
 dnsperf -s 10.10.0.50 -d ~/query-file-example-current -c 200 -T 10 -l 300 -q 10000 -Q 1000
 
 
+# Webflow
+./slowhttptest -c 1000 -B -g -o my_body_stats -i 110 -r 200 -s 8192 -t FAKEVERB -u https://myseceureserver/resources/loginform.html -x 10 -p 3
+
+
