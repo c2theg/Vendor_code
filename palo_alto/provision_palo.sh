f98be6dbb9e0dd4c239b581f13219daf38e38a05
Chris Gray <christophermjgray@gmail.com>
2018-05-01 16:15:09 -0400

    Update provision_palo.sh

--- palo_alto/provision_palo.sh
+++ palo_alto/provision_palo.sh
@@ -18,13 +18,12 @@ echo "
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
 \r\n \r\n
-Version:  0.0.1                             \r\n
+Version:  0.0.3                             \r\n
 Last Updated:  5/1/2018
 \r\n \r\n
 This is meant for Ubuntu 16.04+  \r\n \r\n"
 
 #---------------------------------------------------------------------------------------------------------
-
 if [ -s "logstash_palo.conf" ]
 then
 	echo "Deleting old configs...  "
@@ -34,11 +33,13 @@ fi
 echo "Downloading ES 6 Mappings Config"
 wget -O "palo_es6-template.json" "https://raw.githubusercontent.com/c2theg/Vendor_code/master/palo_alto/palo_es6-template.json"
 wait
-
 #---------------------------------------------------------------------
 echo "\r\n \r\n Downloading Logstash Config"
 wget -O "logstash_palo.conf" "https://raw.githubusercontent.com/c2theg/Vendor_code/master/palo_alto/logstash_palo.conf"
 wget -O "custom_grok_patterns.yml" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/ES/custom_grok_patterns.yml"
+
+wget -O "logrotate_syslog_PA_net.conf" "https://raw.githubusercontent.com/c2theg/Vendor_code/master/palo_alto/logrotate_syslog_net.conf"
+wget -O "logrotate_syslog_PA_url.conf" "https://raw.githubusercontent.com/c2theg/Vendor_code/master/palo_alto/logrotate_syslog_url.conf"
 
 echo "\r\n \r\n"
 echo "moving configs....  "
@@ -55,36 +56,22 @@ then
 	mkdir logstash
 fi
 sudo mv "logstash_palo.conf"  "logstash/logstash_palo.conf"
-
-
 sudo chmod -R 755 /var/lib/logstash
 sudo chown -R logstash:logstash /var/lib/logstash
-echo "Done! \r\n "
-#---------------------------------------------------------------------
-
-bin/logstash-plugin list --verbose
-
-if [ -s "logstash/logstash_palo.conf" ] 
-then
-	#echo "\r\n \r\n  Starting Logstash (this may take 1 minute)... \r\n \r\n"
-	#sudo -u logstash /usr/share/logstash/bin/logstash --path.settings=/etc/logstash -f logstash/f5_logging.conf
-	#sudo /usr/share/logstash/bin/logstash --path.settings=/etc/logstash -f /home/ubuntu/logstash/f5_logging.conf
-	
-	echo "To start logstash use the following: \r\n \r\n
-	   sudo  /usr/share/logstash/bin/logstash --path.settings=/etc/logstash -f /home/ubuntu/logstash/logstash_palo.conf 
-	   
-	\r\n \r\n"
-	wait
-fi
 
 #-------------------------------------------------------
 #echo "\r\n \r\n Downloading Syslog_ng Config"
 #sudo chown -R syslog:adm /var/log/bigip
 wget -O "syslog-ng_palo.conf" "https://raw.githubusercontent.com/c2theg/Vendor_code/master/palo_alto/syslog-ng_palo.conf"
 sudo cp "syslog-ng_palo.conf" "/etc/syslog-ng/conf.d/syslog-ng_palo.conf"
-sudo -u /etc/init.d/syslog-ng restart
+#sudo -u /etc/init.d/syslog-ng restart
+sudo service syslog-ng restart
 #wait
 #-------------------------------------------------------
+
+#---- Logrotate ----
+sudo mv "logrotate_syslog_PA_net.conf" "/etc/logrotate.d/logrotate_syslog_PA_net.conf"
+sudo mv "logrotate_syslog_PA_url.conf" "/etc/logrotate.d/logrotate_syslog_PA_url.conf"
 
 #----------- Update ES config --------------------------------------------
 mv /etc/elasticsearch/elasticsearch.yml  /etc/elasticsearch/elasticsearch6_backup.yml
@@ -106,12 +93,25 @@ echo "\r\n \r\n Downloading Update ELK plugins shell script.. "
 wget -O "update_elk_plugins.sh" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_elk_plugins.sh"
 sudo chmod u+x update_elk_plugins.sh
 wait
-
+#-------------------------------------------------------
+echo "\r\n \r\n Logstash plugins: \r\n \r\n"
+bin/logstash-plugin list --verbose
 
 echo "\r\n \r\n To update your logstash plugins: \r\n \r\n
 	sudo ./update_elk_plugins.sh    \r\n"
 
 sudo ./update_elk_plugins.sh
 
-
+if [ -s "logstash/logstash_palo.conf" ] 
+then
+	#echo "\r\n \r\n  Starting Logstash (this may take 1 minute)... \r\n \r\n"
+	#sudo -u logstash /usr/share/logstash/bin/logstash --path.settings=/etc/logstash -f logstash/f5_logging.conf
+	#sudo /usr/share/logstash/bin/logstash --path.settings=/etc/logstash -f /home/ubuntu/logstash/f5_logging.conf
+	
+	echo "To start logstash use the following: \r\n \r\n
+	   sudo  /usr/share/logstash/bin/logstash --path.settings=/etc/logstash -f /home/ubuntu/logstash/logstash_palo.conf 
+	\r\n \r\n"
+	wait
+fi
+#-------------------------------------------------------
 echo "DONE! \r\n "
