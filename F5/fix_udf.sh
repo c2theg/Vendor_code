907952cfcae4e83652ff1327063ac85b2cd9c66e
Chris Gray <christophermjgray@gmail.com>
2018-10-23 17:03:39 -0400

    Update fix_udf.sh

--- F5/fix_udf.sh
+++ F5/fix_udf.sh
@@ -1,5 +1,21 @@
-# put this in crontab with the following:
-#
-#
 export TERM=xterm
 export EDITOR=nano
+
+# By: Christopher Gray
+# 10/23/18
+# version: 0.0.1
+#------ Auto add to startup ------------------------------------
+Cron_output=$(crontab -l | grep "fix_udf.sh")
+#echo "The output is: [ $Cron_output ]"
+if [ -z "$Cron_output" ]
+then
+    echo "Script not in crontab. Adding."
+ 
+    line="@reboot /root/fix_udf.sh"
+    (crontab -u root -l; echo "$line" ) | crontab -u root -
+    
+    wait
+    /etc/init.d/cron restart  > /dev/null
+else
+    echo "Script was found in crontab. skipping addition"
+fi
