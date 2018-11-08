4fa454bc94369bc38f8d1704fc6f63cde17cc085
Chris Gray <christophermjgray@gmail.com>
2018-11-08 12:01:11 -0500

    Create kill_all_attacks.sh

---
+++ F5/syslog-ng/kill_all_attacks.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Christopher Gray
+# Version 0.0.1
+#  11-8-18
+
+killall -9 dnsperf
+killall -9 hping3
+killall -9 nping
+killall -9 ab
+killall -9 attack_dns_nxdomain.py
+killall -9 attack_phantomdomain.py
