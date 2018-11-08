95d9b80b8502fe4941187f9e48c72b377e04b947
Chris Gray <christophermjgray@gmail.com>
2018-11-08 12:02:43 -0500

    Update update_attacks.sh

--- F5/syslog-ng/update_attacks.sh
+++ F5/syslog-ng/update_attacks.sh
@@ -21,13 +21,13 @@ echo "
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
 \r\n \r\n
-Version:  0.0.8                             \r\n
-Last Updated:  11/7/2018
+Version:  0.0.9                             \r\n
+Last Updated:  11/9/2018
 \r\n \r\n
 This is meant for Ubuntu 16.04+  \r\n \r\n"
 #---------------------------------------------------
 if [ -f update_attacks.sh ]; then
-    rm update_attacks.sh gen_data.sh gen_udp_floods.sh gen_legit_dns_traffic.sh attack_dns_nxdomain.py attack_dns_watertorture_wget.sh attack_phantomdomain.py install_nmap-git.sh
+    rm update_attacks.sh gen_data.sh gen_udp_floods.sh gen_legit_dns_traffic.sh attack_dns_nxdomain.py attack_dns_watertorture_wget.sh attack_phantomdomain.py install_nmap-git.sh kill_all_attacks.sh
 fi
 
 echo "\r\n \r\n Downloading scripts... \r\n"
@@ -39,9 +39,10 @@ wget https://raw.githubusercontent.com/c2theg/Vendor_code/master/F5/syslog-ng/ge
 wget https://raw.githubusercontent.com/c2theg/Vendor_code/master/F5/syslog-ng/attack_dns_nxdomain.py
 wget https://raw.githubusercontent.com/c2theg/Vendor_code/master/F5/syslog-ng/attack_dns_watertorture_wget.sh
 wget https://raw.githubusercontent.com/c2theg/Vendor_code/master/F5/syslog-ng/attack_phantomdomain.py
+wget https://raw.githubusercontent.com/c2theg/Vendor_code/master/F5/syslog-ng/kill_all_attacks.sh
 
 #---- Permissions ---
-chmod u+x update_attacks.sh gen_data.sh gen_udp_floods.sh gen_legit_dns_traffic.sh attack_dns_nxdomain.py attack_dns_watertorture_wget.sh attack_phantomdomain.py
+chmod u+x update_attacks.sh gen_data.sh gen_udp_floods.sh gen_legit_dns_traffic.sh attack_dns_nxdomain.py attack_dns_watertorture_wget.sh attack_phantomdomain.py kill_all_attacks.sh
 
 #---- add auto update to crontab ----
 Cron_output=$(crontab -l | grep "update_attacks.sh")
